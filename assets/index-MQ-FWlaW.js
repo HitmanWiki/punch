b62b61eaa66347be269568db711661c17451277e
HitmanWiki <abhimanyumalhotra.2021@gmail.com>
2026-04-23 20:55:41 +0530

    first commit

--- assets/index-MQ-FWlaW.js
+++ assets/index-MQ-FWlaW.js
@@ -31214,7 +31214,7 @@ const u4 = ({
                         d: "M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.27 6.27 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.34-6.34V8.75a8.18 8.18 0 0 0 3.76.92V6.69Z"
                     })
                 }),
-                href: "https://www.tiktok.com/@ganbare_punch?_r=1&_t=ZS-93nsPfNX65x",
+                href: "https://www.tiktok.com/@kosuke.shikano",
                 color: "bg-forest hover:bg-forest-dark",
                 desc: "Watch Punch's videos"
             }];
